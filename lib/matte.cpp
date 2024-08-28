da5294051130eabeb68df0f6613de4d6e57ed89e
Olle Mattsson <rymdolle@users.noreply.github.com>
2024-08-28 15:07:07 +0200

    Fix add/sub bug

--- lib/matte.cpp
+++ lib/matte.cpp
@@ -1,11 +1,11 @@
 #include "matte.h"
 
 int matte::add(int a, int b) {
-  return a - b;
+  return a + b;
 }
 
 int matte::sub(int a, int b) {
-  return a + b;
+  return a - b;
 }
 
 int matte::mul(int a, int b) {
